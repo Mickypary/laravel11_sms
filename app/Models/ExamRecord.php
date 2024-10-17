a729fa718f052d3f079da383c5b7a63dd716f2a5
Mickypary <mikipary@gmail.com>
2024-10-17 16:35:55 +0100

    Reboot afresh with migrations and seedings. Also updated Model classes

--- app/Models/ExamRecord.php
+++ app/Models/ExamRecord.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class ExamRecord extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['exam_id', 'my_class_id', 'student_id', 'section_id', 'af', 'af_id', 'ps', 'ps_id', 't_comment', 'p_comment', 'year', 'total', 'ave', 'class_ave', 'pos'];
 }
